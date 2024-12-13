d5bb0ade5b932ad4ed21afc6f4bf752162966210
Erfan Abbasi <abbasy.4708@gmail.com>
2024-12-13 17:32:09 +0330

    optimize loop structure and improve grid traversal logic
    - Reordered nested loops to iterate over columns first for better alignment with DP logic.
    - Added boundary checks for the `i` variable to prevent out-of-index errors.
    - Displayed the maximum happiness value from the last cell of the DP array.

--- src/phase1/main.py
+++ src/phase1/main.py
@@ -3,15 +3,27 @@
 def find_max_happiness(grid):
     m, n = len(grid), len(grid[0])
     dp = [[0] * n for _ in range(m)]
-    for i in range(1, m):
-        for j in range(n):
-            dp[i - 1][j] = grid[i][j] + max(dp[i - 1][j], dp[i - 1][j - 1])
-            print(dp[i - 1][j])
+    for c in range(m):
+        dp[c][0] = grid[c][0]
+    for j in range(1, n):
+        for i in range(m):
+            max_prev = 0
+            if 0 < i < m - 1:
+                max_prev = max(dp[i - 1][j - 1], dp[i][j - 1], dp[i + 1][j - 1])
+            elif i == 0:
+                max_prev = max(dp[i][j - 1], dp[i + 1][j - 1])
+            elif i == m - 1:
+                max_prev = max(dp[i - 1][j - 1], dp[i][j - 1])
+
+            dp[i][j] = grid[i][j] + max_prev
+    print(dp[m - 1][n - 1])
 
 
 sample_input = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [3, 2, 1, 7, 8, 9],
+    [4, 5, 6, 1, 3, 10],
+    [7, 8, 9, 2, 5, 3],
+    [6, 5, 4, 9, 8, 7],
+    [1, 1, 1, 1, 1, 22]
 ]
 find_max_happiness(sample_input)
